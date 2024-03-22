d84203648a937be32a392b31acbe49d91fe33d26
Smaqiola <marieacholaa@gmail.com>
2024-03-22 04:58:49 +0300

    Complete Arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+
+const multiply = num1 * num2;
+console.log(multiply); 
+
+let num3 = 24;
+let num4 = 5;
+
+const mod = num3 % num4;
+console.log(mod);
+
+const random = Math.round(Math.random() * 10); 
+console.log(random);
+
+const max = Math.max(4,8,12,16,20);
+console.log(max);
+
+
+
+
+
+
